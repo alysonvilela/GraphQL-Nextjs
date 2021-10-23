e7456c58eb2ddf08c48b1b85ad4d98a338cc980b
alysonvilela <alysoonvila2@gmail.com>
2021-10-23 15:01:45 -0300

    feat(*): Add radix colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>GraphQBLOG</title>
+        <title>Finances</title>
         <link rel="shortcut icon" href="/img/img-512.png" />
         <link rel="apple-touch-icon" href="/img/img-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project to study a GraphCMS integration with Nextjs and StyledComponents"
+          content="A free app for personal finance managements"
         />
       </Head>
       <GlobalStyle />
